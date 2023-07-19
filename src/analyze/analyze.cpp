5933c196809dceeaeefa8e6625760e2668a3a84d
Titanium <titanium@T1T4M1UN-0x00>
2023-07-19 22:52:29 +0800

    修复了cherry-pick中未选中的datetime bug，review了analyze中的代码

--- src/analyze/analyze.cpp
+++ src/analyze/analyze.cpp
@@ -85,7 +85,7 @@ std::shared_ptr<Query> Analyze::do_analyze(std::shared_ptr<ast::TreeNode> parse)
             Value val = convert_sv_value(set_clause->val);
             auto col = table.get_col(set_clause->col_name);
             ColType lhs_type = col->type;
-            ColType &rhs_type = val.type;
+            ColType rhs_type = val.type;
             if (!is_compatible_type(lhs_type, rhs_type)) {
                 throw IncompatibleTypeError(coltype2str(lhs_type), coltype2str(rhs_type));
             }
