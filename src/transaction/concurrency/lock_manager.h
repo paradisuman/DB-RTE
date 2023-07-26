f635f182682f79f9fa81c4199c64ea2154906630
Paradisum <2513209473@qq.com>
2023-07-27 02:59:33 +0800

    初始化了share

--- src/transaction/concurrency/lock_manager.h
+++ src/transaction/concurrency/lock_manager.h
@@ -40,7 +40,8 @@ class LockManager {
         std::list<LockRequest> request_queue_;  // 加锁队列
         std::condition_variable cv_;            // 条件变量，用于唤醒正在等待加锁的申请，在no-wait策略下无需使用
         GroupLockMode group_lock_mode_ = GroupLockMode::NON_LOCK;   // 加锁队列的锁模式
-        txn_id_t  first_share;
+        // 空值为-1，
+        txn_id_t  first_share = -1;
     };
 
 public:
