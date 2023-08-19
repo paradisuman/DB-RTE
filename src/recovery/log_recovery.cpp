d4528b6d2d38a70318fb6e45d439429d6df13869
Titanium <titanium@T1T4M1UN-0x00>
2023-08-19 13:51:44 +0800

    排除coredump引入: add log add lsn

--- src/recovery/log_recovery.cpp
+++ src/recovery/log_recovery.cpp
@@ -97,9 +97,9 @@ void RecoveryManager::analyze() {
                         redo_log_in_page.redo_logs_.push_back(table_file->get_page_lsn(rid.page_no));
                     }
                     auto &redo_log_in_page = dirty_page_table[page_id];
-                //     if (redo_log_in_page.redo_logs_[0] < insert_log_record->lsn_) {
+                    if (redo_log_in_page.redo_logs_[0] < insert_log_record->lsn_) {
                 //         redo_log_in_page.redo_logs_.push_back(insert_log_record->lsn_);
-                //     }
+                    }
                     // 更新 lsn2log
                     lsn2log[insert_log_record->lsn_] = std::move(insert_log_record);
 
