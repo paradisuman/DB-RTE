9cbb0a3eec73815bdaf7e1423602305e0c306388
Titanium <titanium@T1T4M1UN-0x00>
2023-08-19 14:05:11 +0800

    排除coredump引入: crud lod init dpt

--- src/recovery/log_recovery.cpp
+++ src/recovery/log_recovery.cpp
@@ -123,13 +123,13 @@ void RecoveryManager::analyze() {
                     auto table_fd = table_file->GetFd();
                     const auto page_id = PageId {table_fd, rid.page_no};
 
-                //     if (dirty_page_table.count(page_id) == 0) {
-                //         auto itr = dirty_page_table.emplace(page_id, RedoLogsInPage());
-                //         auto &redo_log_in_page = itr.first->second;
-                //         redo_log_in_page.table_file_ = table_file;
-                //         redo_log_in_page.redo_logs_.push_back(table_file->get_page_lsn(rid.page_no));
-                //     }
-                //     auto &redo_log_in_page = dirty_page_table[page_id];
+                    if (dirty_page_table.count(page_id) == 0) {
+                        auto itr = dirty_page_table.emplace(page_id, RedoLogsInPage());
+                        auto &redo_log_in_page = itr.first->second;
+                        redo_log_in_page.table_file_ = table_file;
+                        redo_log_in_page.redo_logs_.push_back(table_file->get_page_lsn(rid.page_no));
+                    }
+                    auto &redo_log_in_page = dirty_page_table[page_id];
                 //     if (redo_log_in_page.redo_logs_[0] < delete_log_record->lsn_) {
                 //         redo_log_in_page.redo_logs_.push_back(delete_log_record->lsn_);
                 //     }
@@ -154,13 +154,13 @@ void RecoveryManager::analyze() {
                     auto table_fd = table_file->GetFd();
                     const auto page_id = PageId {table_fd, rid.page_no};
 
-                //     if (dirty_page_table.count(page_id) == 0) {
-                //         auto itr = dirty_page_table.emplace(page_id, RedoLogsInPage());
-                //         auto &redo_log_in_page = itr.first->second;
-                //         redo_log_in_page.table_file_ = table_file;
-                //         redo_log_in_page.redo_logs_.push_back(table_file->get_page_lsn(rid.page_no));
-                //     }
-                //     auto &redo_log_in_page = dirty_page_table[page_id];
+                    if (dirty_page_table.count(page_id) == 0) {
+                        auto itr = dirty_page_table.emplace(page_id, RedoLogsInPage());
+                        auto &redo_log_in_page = itr.first->second;
+                        redo_log_in_page.table_file_ = table_file;
+                        redo_log_in_page.redo_logs_.push_back(table_file->get_page_lsn(rid.page_no));
+                    }
+                    auto &redo_log_in_page = dirty_page_table[page_id];
                 //     if (redo_log_in_page.redo_logs_[0] < update_log_record->lsn_) {
                 //         redo_log_in_page.redo_logs_.push_back(update_log_record->lsn_);
                 //     }
