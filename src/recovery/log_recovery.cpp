6e0f959fc22aab4ff1bd17043b222be5650cc580
Titanium <titanium@T1T4M1UN-0x00>
2023-08-19 16:02:06 +0800

    排除coredump引入: 修改了判断插入lsn的逻辑

--- src/recovery/log_recovery.cpp
+++ src/recovery/log_recovery.cpp
@@ -89,14 +89,23 @@ void RecoveryManager::analyze() {
                     auto table_fd = table_file->GetFd();
                     const auto page_id = PageId {table_fd, rid.page_no};
 
-                    if (dirty_page_table.count(page_id) == 0) {
-                        auto itr = dirty_page_table.emplace(page_id, RedoLogsInPage());
-                        auto &redo_log_in_page = itr.first->second;
+                    // if (dirty_page_table.count(page_id) == 0) {
+                    //     auto itr = dirty_page_table.emplace(page_id, RedoLogsInPage());
+                    //     auto &redo_log_in_page = itr.first->second;
+                    //     redo_log_in_page.table_file_ = table_file;
+                    //     redo_log_in_page.redo_logs_.push_back(table_file->get_page_lsn(rid.page_no));
+                    // } 
+                    // auto &redo_log_in_page = dirty_page_table[page_id];
+                    // if (redo_log_in_page.redo_logs_.empty() || redo_log_in_page.redo_logs_[0] < insert_log_record->lsn_) {
+                    //     redo_log_in_page.redo_logs_.push_back(insert_log_record->lsn_);
+                    // }
+                    auto itr = dirty_page_table.emplace(page_id, RedoLogsInPage());
+                    auto &redo_log_in_page = itr.first->second;
+                    if (itr.second) {
                         redo_log_in_page.table_file_ = table_file;
                         redo_log_in_page.redo_logs_.push_back(table_file->get_page_lsn(rid.page_no));
-                    } 
-                    auto &redo_log_in_page = dirty_page_table[page_id];
-                    if (redo_log_in_page.empty() || redo_log_in_page.redo_logs_[0] < insert_log_record->lsn_) {
+                    }
+                    if (redo_log_in_page.redo_logs_[0] < insert_log_record->lsn_) {
                         redo_log_in_page.redo_logs_.push_back(insert_log_record->lsn_);
                     }
                     // redo_log_in_page.redo_logs_.push_back(insert_log_record->lsn_);
